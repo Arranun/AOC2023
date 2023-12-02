2e0b106d2399d6fb801f499814dc0d99977358a2
Arranun <karliosos65@googlemail.com>
2023-12-02 01:48:04 +0100

    Day 1 Part 2

--- day1/day1.go
+++ day1/day1.go
@@ -11,10 +11,15 @@ import (
 func main() {
 	args := os.Args[1:]
 	lines := helper.ReadTextFile(args[0])
+	part1(lines)
+	part2(lines)
+}
+
+func part1(lines []string) {
 	var linesOnlyNumbers []string
 	for _, l := range lines {
 		var numbers string
-		for _, c := range writtenNumberToDigit(l) {
+		for _, c := range l {
 			if c > 48 && c < 59 {
 				numbers = numbers + string(c)
 			}
@@ -22,15 +27,34 @@ func main() {
 		linesOnlyNumbers = append(linesOnlyNumbers, numbers)
 	}
 	var sum int
-	for i, l := range linesOnlyNumbers {
-		fmt.Printf("%s : %s \n", lines[i], l)
+	for _, l := range linesOnlyNumbers {
 		sum += helper.RemoveError(strconv.Atoi(l[:1] + l[len(l)-1:]))
 	}
 	fmt.Println(sum)
 }
 
-func writtenNumberToDigit(input string) string {
-	r := strings.NewReplacer(
+func part2(lines []string) {
+	var sum int
+	for _, l := range lines {
+		var firstDigit = ""
+		var lastDigit = ""
+		var index = 0
+		for firstDigit == "" {
+			firstDigit = searchDigits(l[:index])
+			index++
+		}
+		index = 0
+		for lastDigit == "" {
+			lastDigit = searchDigits(l[len(l)-index:])
+			index++
+		}
+		sum += helper.RemoveError(strconv.Atoi(firstDigit + lastDigit))
+	}
+	fmt.Println(sum)
+}
+
+func searchDigits(input string) string {
+	var digits = []string{
 		"one", "1",
 		"two", "2",
 		"three", "3",
@@ -39,6 +63,16 @@ func writtenNumberToDigit(input string) string {
 		"six", "6",
 		"seven", "7",
 		"eight", "8",
-		"nine", "9")
-	return r.Replace(input)
+		"nine", "9",
+	}
+	for i, d := range digits {
+		if strings.Contains(input, d) {
+			if i%2 == 0 {
+				return digits[i+1]
+			}
+			return digits[i]
+
+		}
+	}
+	return ""
 }
