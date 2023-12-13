58a19a032e169d8990464227ed355544bb29ba0d
Arranun <karliosos65@googlemail.com>
2023-12-13 20:58:27 +0100

    Revert "Day 12 Part 2 Temp"
    This reverts commit 89e58bb33a8d5c7e5e8fa3697d652a8be77d7057.

--- day12/day12.go
+++ day12/day12.go
@@ -19,12 +19,6 @@ type Row struct {
 type Path struct {
 	currentGroupIndex int
 	remainBackup      []int
-	amount            int
-}
-
-type GroupVariants struct {
-	backup []int
-	amount int
 }
 
 func main() {
@@ -35,7 +29,7 @@ func main() {
 	if testBool {
 		test(lines)
 	} else {
-		part1(lines)
+		part2(lines)
 	}
 
 	elapsed := time.Since(start)
@@ -57,7 +51,7 @@ func part1(lines []string) {
 	groupVariantMap := getGroupVariants(rows)
 	sum := 0
 	for _, r := range rows {
-		possiblePaths := getPossibleVariations(r, &groupVariantMap)
+		possiblePaths := getPossibleVariations(r, groupVariantMap)
 		sum += possiblePaths
 	}
 	fmt.Println(sum)
@@ -73,12 +67,12 @@ func getRows(lines []string) []Row {
 	return rows
 }
 
-func getGroupVariants(rows []Row) map[string][]GroupVariants {
-	groupVariantMap := map[string][]GroupVariants{}
+func getGroupVariants(rows []Row) map[string][][]int {
+	groupVariantMap := map[string][][]int{}
 	for _, r := range rows {
 		for _, g := range r.groups {
 			_, ok := groupVariantMap[g]
-			if !ok && len(r.groups) > 1 {
+			if !ok {
 				groupVariantMap[g] = getVariationsOfGroup(g)
 			}
 
@@ -99,18 +93,18 @@ func test(lines []string) {
 	groupVariantMap := getGroupVariants(rows)
 	sum := 0
 	for i, r := range rows {
-		possiblePaths := getPossibleVariations(r, &groupVariantMap)
+		possiblePaths := getPossibleVariations(r, groupVariantMap)
 		sum += possiblePaths
 		if !(possiblePaths == expection[i]) {
 			fmt.Printf("%s: %d \n", lines[i], possiblePaths)
-			possiblePaths = getPossibleVariations(r, &groupVariantMap)
+			possiblePaths = getPossibleVariations(r, groupVariantMap)
 		}
 	}
 	fmt.Println(sum)
 }
 
-func getPossibleVariations(row Row, groupVariantMap *map[string][]GroupVariants) int {
-	activePaths := []Path{{0, row.backup, 1}}
+func getPossibleVariations(row Row, groupVariantMap map[string][][]int) int {
+	activePaths := []Path{{0, row.backup}}
 	possiblePaths := []Path{}
 	for len(activePaths) > 0 {
 		currentPath := activePaths[0]
@@ -118,55 +112,42 @@ func getPossibleVariations(row Row, groupVariantMap *map[string][]GroupVariants)
 		if currentPath.currentGroupIndex == len(row.groups) && len(currentPath.remainBackup) == 0 {
 			possiblePaths = append(possiblePaths, currentPath)
 		} else if !(currentPath.currentGroupIndex == len(row.groups)) {
-			groupVars := (*groupVariantMap)[row.groups[currentPath.currentGroupIndex]]
+			groupVars := groupVariantMap[row.groups[currentPath.currentGroupIndex]]
 			for _, gv := range groupVars {
-				possible, _ := groupVarPossible(gv.backup, currentPath.remainBackup)
-				if possible {
+				if groupVarPossible(gv, currentPath.remainBackup) {
 					zeroesInGv := 0
-					for _, v := range gv.backup {
+					for _, v := range gv {
 						if v == 0 {
 							zeroesInGv++
 						}
 					}
 					newActivePath := Path{}
 					newActivePath.currentGroupIndex = currentPath.currentGroupIndex + 1
-					newActivePath.remainBackup = currentPath.remainBackup[len(gv.backup)-zeroesInGv:]
-					newActivePath.amount = currentPath.amount * gv.amount
+					newActivePath.remainBackup = currentPath.remainBackup[len(gv)-zeroesInGv:]
 					activePaths = append(activePaths, newActivePath)
 				}
 			}
 		}
 	}
-	sum := 0
-	for _, p := range possiblePaths {
-		sum += p.amount
-	}
-	return sum
+	return len(possiblePaths)
 }
 
-func groupVarPossible(groupVar []int, remainingBackup []int) (bool, []int) {
-	if remainingBackup[0] == 0 {
-		if groupVar[0] != 0 {
-			return false, remainingBackup
-		}
-		remainingBackup = remainingBackup[1:]
-		groupVar = groupVar[1:]
+func groupVarPossible(groupVar []int, remainingBackup []int) bool {
+	if len(groupVar) == 1 && groupVar[0] == 0 && len(remainingBackup) == 0 {
+		return true
 	}
-
 	if len(groupVar) > len(remainingBackup) {
-		return false, remainingBackup
+		return false
 	}
-	index := 0
-	for _, gv := range groupVar {
-		if gv != 0 && gv != remainingBackup[index] {
-			return false, remainingBackup
+	for i, gv := range groupVar {
+		if gv != 0 && gv != remainingBackup[i] {
+			return false
 		}
-		index++
 	}
-	return true, remainingBackup[index:]
+	return true
 }
 
-func getVariationsOfGroup(group string) []GroupVariants {
+func getVariationsOfGroup(group string) [][]int {
 	unknownIndex := []int{}
 	for i, c := range group {
 		if c == '?' {
@@ -174,26 +155,19 @@ func getVariationsOfGroup(group string) []GroupVariants {
 		}
 	}
 	variantsLen := int(math.Pow(2, float64(len(unknownIndex))))
-	groupVariants := []GroupVariants{}
-	groupVariantsMap := map[string]int{}
+	binaryVariations := make([][]bool, variantsLen)
 	for i := 0; i < variantsLen; i++ {
 		binary := fmt.Sprintf("%0*b", len(unknownIndex), i)
-		bv := make([]bool, len(unknownIndex))
+		binaryVariations[i] = make([]bool, len(unknownIndex))
 		for j, b := range binary {
 			if b == '1' {
-				bv[j] = true
+				binaryVariations[i][j] = true
 			}
 		}
-		backupFromGroupVariant := getBackupFromGroupVariant(group, unknownIndex, bv)
-		groupVariantsMap[strings.Trim(strings.Join(strings.Fields(fmt.Sprint(backupFromGroupVariant)), ","), "[]")]++
 	}
-	for k, v := range groupVariantsMap {
-		backup := strings.Split(k, ",")
-		tempVariant := make([]int, len(backup))
-		for i, n := range backup {
-			tempVariant[i] = helper.RemoveError(strconv.Atoi(n))
-		}
-		groupVariants = append(groupVariants, GroupVariants{tempVariant, v})
+	groupVariants := [][]int{}
+	for _, bv := range binaryVariations {
+		groupVariants = append(groupVariants, getBackupFromGroupVariant(group, unknownIndex, bv))
 	}
 
 	return groupVariants
@@ -217,25 +191,16 @@ func getBackupFromGroupVariant(group string, unknownIndex []int, binary []bool) 
 		if r == '#' {
 			currentBackup++
 		}
-		if r == '.' {
+		if r == '.' && currentBackup > 0 {
 			backup = append(backup, currentBackup)
 			currentBackup = 0
 		}
 	}
-	backup = append(backup, currentBackup)
-	lastN := -1
-	tmpBackup := []int{}
-	for _, n := range backup {
-		if n != 0 {
-			tmpBackup = append(tmpBackup, n)
-		}
-		if n == 0 && lastN != 0 {
-			tmpBackup = append(tmpBackup, n)
-		}
-		lastN = n
+	if currentBackup != 0 || len(backup) == 0 {
+		backup = append(backup, currentBackup)
 	}
 
-	return tmpBackup
+	return backup
 }
 
 func getBackupFromGroup(group string) []int {
