2b297b2557b3a246bd261df588289a1d386f5146
Arranun <karliosos65@googlemail.com>
2023-12-15 00:14:45 +0100

    Day 14 Part 2

--- day14/day14.go
+++ day14/day14.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -12,18 +13,109 @@ func main() {
 	args := os.Args[1:]
 	lines := helper.ReadTextFile(args[0])
 	start := time.Now()
-	rowLoad := make([]int, len(lines))
-	tiltNorth(lines, rowLoad)
-	var sum int
-	for i, r := range rowLoad {
-		sum += r * (len(rowLoad) - i)
+	scores := map[string]int{}
+	var cycle int
+	for i := 0; i < 1000; i++ {
+		lines = step(lines)
+		key := getKey(lines)
+		if scores[key] > 0 {
+			cycle = i - scores[key]
+		} else {
+			scores[key] = i
+		}
 	}
+	lines = helper.ReadTextFile(args[0])
+	for i := 0; i < (1000000000 % cycle); i++ {
+		lines = step(lines)
+	}
+	sum := getScore(lines)
 	fmt.Println(sum)
 	elapsed := time.Since(start)
 	log.Printf("Took %s", elapsed)
 }
 
-func tiltNorth(lines []string, rowLoad []int) {
+func step(lines []string) []string {
+	lines = tiltNorth(lines)
+	lines = tiltWest(lines)
+	lines = tiltSouth(lines)
+	lines = tiltEast(lines)
+	return lines
+}
+
+func getKey(lines []string) string {
+	line := ""
+	for _, l := range lines {
+		line += l
+	}
+	return line
+}
+
+func getScore(lines []string) int {
+	var sum int
+	for i, l := range lines {
+		sum += (len(lines) - i) * strings.Count(l, "O")
+	}
+	return sum
+}
+
+func compareLines(lines1, lines2 []string) bool {
+	for i, l := range lines1 {
+		if lines2[i] != l {
+			return false
+		}
+	}
+	return true
+}
+
+func tiltWest(lines []string) []string {
+	tmpLines := make([]string, len(lines))
+	for i, l := range lines {
+		tmpLines[i] = l
+	}
+	for i := 0; i < len(lines); i++ {
+		currentGround := -1
+		for j := 0; j < len(lines[0]); j++ {
+			switch lines[i][j] {
+			case '#':
+				currentGround = j
+			case 'O':
+				tmpLines[i] = tmpLines[i][:j] + "." + tmpLines[i][j+1:]
+				tmpLines[i] = tmpLines[i][:currentGround+1] + "O" + tmpLines[i][currentGround+2:]
+				currentGround++
+			}
+		}
+	}
+	return tmpLines
+}
+
+func tiltEast(lines []string) []string {
+	tmpLines := make([]string, len(lines))
+	for i, l := range lines {
+		tmpLines[i] = l
+	}
+	for i := 0; i < len(lines); i++ {
+		currentGround := len(lines[0])
+		for j := len(lines[0]) - 1; j > -1; j-- {
+			switch lines[i][j] {
+			case '#':
+				currentGround = j
+			case 'O':
+				tmpLines[i] = tmpLines[i][:j] + "." + tmpLines[i][j+1:]
+				part1 := tmpLines[i][:currentGround-1]
+				part2 := tmpLines[i][currentGround:]
+				tmpLines[i] = part1 + "O" + part2
+				currentGround--
+			}
+		}
+	}
+	return tmpLines
+}
+
+func tiltNorth(lines []string) []string {
+	tmpLines := make([]string, len(lines))
+	for i, l := range lines {
+		tmpLines[i] = l
+	}
 	for i := 0; i < len(lines[0]); i++ {
 		currentGroundRow := -1
 		for j := 0; j < len(lines); j++ {
@@ -31,9 +123,32 @@ func tiltNorth(lines []string, rowLoad []int) {
 			case '#':
 				currentGroundRow = j
 			case 'O':
-				rowLoad[currentGroundRow+1]++
+				tmpLines[j] = tmpLines[j][:i] + "." + tmpLines[j][i+1:]
+				tmpLines[currentGroundRow+1] = tmpLines[currentGroundRow+1][:i] + "O" + tmpLines[currentGroundRow+1][i+1:]
 				currentGroundRow++
 			}
 		}
 	}
+	return tmpLines
+}
+
+func tiltSouth(lines []string) []string {
+	tmpLines := make([]string, len(lines))
+	for i, l := range lines {
+		tmpLines[i] = l
+	}
+	for i := 0; i < len(lines[0]); i++ {
+		currentGroundRow := len(lines)
+		for j := len(lines) - 1; j > -1; j-- {
+			switch lines[j][i] {
+			case '#':
+				currentGroundRow = j
+			case 'O':
+				tmpLines[j] = tmpLines[j][:i] + "." + tmpLines[j][i+1:]
+				tmpLines[currentGroundRow-1] = tmpLines[currentGroundRow-1][:i] + "O" + tmpLines[currentGroundRow-1][i+1:]
+				currentGroundRow--
+			}
+		}
+	}
+	return tmpLines
 }
